e5fd6d23d2e4c3652f4b304f405038ea03adc68d
nathanCMonte <nathannose7@gmail.com>
2025-09-03 11:18:49 -0300

    fisica bola e script personagem
    terminei a fisica da bola e inicio do script de pegar ela e segurar

--- projeto-dunkmaster/bola.gd
+++ projeto-dunkmaster/bola.gd
@@ -1,35 +1,27 @@
 extends RigidBody2D
 
-const SPEED = 300.0
-var JUMP_VELOCITY = -400.0
-@onready var raycast_down = $RayCastbaixo
-@onready var raycast_esquerda = $RayCastesquerda
-@onready var raycast_direita = $RayCastdireita
+const GRAVITY = 400.0
+const BOUNCE_FORCE_VERTICAL = -500.0
+const BOUNCE_FORCE_HORIZONTAL = 300.0
+
+@onready var area_de_colisao = $Area2D
+
+func _ready():
+	# Conectar sinal para detectar entrada de corpo na área
+	area_de_colisao.body_entered.connect(_on_body_entered)
 
 func _physics_process(delta: float) -> void:
-	# Aplicar gravidade manual, se necessário
-	if not raycast_down.is_colliding():
-		linear_velocity.y += 400 * delta
-	
-	if Input.is_action_just_pressed("bola_quicar"):
-		JUMP_VELOCITY = -400.0
-		linear_velocity.y = JUMP_VELOCITY
-	
-	# Rebater ao tocar na parede
-	if raycast_direita.is_colliding():
-		linear_velocity.x = -linear_velocity.x
-	
-	if raycast_esquerda.is_colliding():
-		linear_velocity.x = -linear_velocity.x
-		
-	if raycast_down.is_colliding():
-		linear_velocity.y = JUMP_VELOCITY
-		
-#func quicar_nochao(body: Node2D) -> void:
-	#if raycast_down.is_colliding():
-	#if JUMP_VELOCITY < 0:
-		#JUMP_VELOCITY += 40  # Reduz a força do próximo salto
-	#linear_velocity.y = JUMP_VELOCITY
-	#print(JUMP_VELOCITY)
-	#print(body)
-	
+	# Aplicar gravidade manualmente
+	linear_velocity.y += GRAVITY * delta
+
+func _on_body_entered(body):
+	if body.is_in_group("chao"):
+		# Quicar pra cima
+		linear_velocity.y = BOUNCE_FORCE_VERTICAL
+
+	elif body.is_in_group("parede"):
+		# Quicar pro lado oposto ao movimento atual
+		if linear_velocity.x > 0:
+			linear_velocity.x = -BOUNCE_FORCE_HORIZONTAL
+		else:
+			linear_velocity.x = BOUNCE_FORCE_HORIZONTAL
--- projeto-dunkmaster/character_body_2d.gd
+++ projeto-dunkmaster/character_body_2d.gd
@@ -1,46 +1,61 @@
 extends CharacterBody2D
 
+@onready var personagem = $"."
 @onready var animacoes = $AnimatedSprite2D
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -600
-var is_jumping= false
+
+var offset_x = 0
+var is_jumping = false
+var bola_colidida: Node2D = null
+var segurando_bola = false
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 1.5
+
 	movimentar_vertical()
 	movimentar_horizontal()
 	Mudar_animacoes()
 	move_and_slide()
 
+	# Pegar bola
+	if bola_colidida and Input.is_action_just_pressed("driblaEpega"):
+		segurando_bola = true
 
+	# Atualizar posição da bola se estiver segurando
+	if segurando_bola and bola_colidida:
+		var offset_x = 50
+		if animacoes.flip_h:
+			offset_x = -50
 
-
+		var offset = Vector2(offset_x, 0)
+		bola_colidida.global_position = personagem.global_position + offset
 
 func movimentar_vertical():
 	if is_jumping and is_on_floor():
-		is_jumping=false
+		is_jumping = false
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		is_jumping= true
+		is_jumping = true
 		animacoes.play("jump")
 
 func movimentar_horizontal():
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		if direction == 1:
-			animacoes.flip_h = false
-		elif direction == -1:
-			animacoes.flip_h = true
-			
+		animacoes.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
 
 func Mudar_animacoes():
-	if is_jumping == false:
+	if not is_jumping:
 		if velocity.x == 0:
 			animacoes.play("default")
-		elif velocity.x != 0:
+		else:
 			animacoes.play("run")
+
+func _on_frente_body_entered(body: Node2D) -> void:
+	if body.is_in_group("bola"):
+		bola_colidida = body
