1d020912c5eb91af92b88ce66041741259cecf4c
nathanCMonte <nathannose7@gmail.com>
2025-10-29 11:47:04 -0300

    sistema de pause
    sim

---
+++ projeto-dunkmaster/pause_menu.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+var esta_pausado = false
+@onready var painel_pause: CanvasLayer = $"."
+
+func _ready() -> void:
+	painel_pause.visible = false
+	get_tree().paused = false
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("menu"):
+		if not esta_pausado:
+			get_tree().paused = true
+			esta_pausado = true
+			painel_pause.visible = true
+		else:
+			get_tree().paused = false
+			esta_pausado = false
+			painel_pause.visible = false
